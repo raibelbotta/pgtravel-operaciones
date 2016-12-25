aefe8eb3ef376ddcf66af886cae7730830dc65b8
Rabel Botta <raibelbotta@gmail.com>
2016-12-25 14:38:56 -0500

    fix callendar events

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -15,25 +16,66 @@ class DefaultController extends Controller
      * @Method({"get"})
      * @return Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        $from = new \DateTime('first day of this month');
-        $to = new \DateTime('last day of this month');
+        return $this->render('Default/index.html.twig');
+    }
+
+    /**
+     * @Route("/get-calendar-events")
+     * @Method({"get"})
+     * @param Request $request
+     * @return JsonParam
+     */
+    public function getEventsAction(Request $request)
+    {
+        $from = \DateTime::createFromFormat('U', $request->get('start'));
+        $to = \DateTime::createFromFormat('U', $request->get('end'));
 
         $manager = $this->getDoctrine()->getManager();
-        $query = $manager->createQuery('SELECT e FROM AppBundle:Reservation e WHERE e.state = :state AND e.isCancelled = :false')
-                ->setParameters(array(
-                    'state' => \AppBundle\Entity\Reservation::STATE_RESERVATION,
-                    'false' => false
-                ))
+        $qb = $manager->getRepository('AppBundle:Reservation')
+                ->createQueryBuilder('e')
+                ->add('select', '(SELECT MIN(rs1.startAt) FROM AppBundle:ReservationService rs1'
+                        . ' JOIN rs1.reservation r1 WHERE r1.id = e.id) AS startAt', true)
+                ->add('select', '(SELECT MAX(rs2.endAt) FROM AppBundle:ReservationService rs2'
+                        . ' JOIN rs2.reservation r2 WHERE r2.id = e.id) AS endAt', true)
                 ;
-        
-        return $this->render('Default/index.html.twig', array(
-            'events' => array_map(function(\AppBundle\Entity\Reservation $record) {
+        $andX = $qb->expr()->andX(
+                $qb->expr()->eq('e.state', $qb->expr()->literal(\AppBundle\Entity\Reservation::STATE_RESERVATION)),
+                $qb->expr()->eq('e.isCancelled', $qb->expr()->literal(false))
+                );
+
+        $having = $qb->expr()->orX(
+                $qb->expr()->andX(
+                        $qb->expr()->lt('startAt', ':from'),
+                        $qb->expr()->gt('endAt', ':to')
+                        ),
+                $qb->expr()->andX(
+                        $qb->expr()->gte('startAt', ':from'),
+                        $qb->expr()->lte('startAt', ':to')
+                        ),
+                $qb->expr()->andX(
+                        $qb->expr()->gte('endAt', ':from'),
+                        $qb->expr()->lte('endAt', ':to')
+                        )
+                );
+
+        $query = $qb->where($andX)->having($having)->getQuery()->setParameters(array(
+            'from' => $from->format('Y-m-d'),
+            'to' => $to->format('Y-m-d 23:59:59')
+        ));
+
+        #echo $query->getSql(); die();
+        $backColors = array('#ff7d71', '#7dff71', '#7d71ff', 'red', 'blue', 'olive');
+
+        return new JsonResponse(array(
+            'events' => array_map(function(array $record) use ($backColors) {
+
                 return array(
-                    'title' => $record->getName(),
-                    'start' => $record->getStartAt()->format('Y-m-d'),
-                    'end' => $record->getEndAt()->format('Y-m-d')
+                    'title' => $record[0]->getName(),
+                    'start' => date_create($record['startAt'])->format('Y-m-d'),
+                    'end' => date_create($record['endAt'])->format('Y-m-d'),
+                    'backgroundColor' => $backColors[rand(0, count($backColors) - 1)]
                 );
             }, $query->getResult())
         ));
